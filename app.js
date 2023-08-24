4e0cfcccc52f6acea036b63417bda169544de39f
felipehmora <felipehmoraarg@gmail.com>
2023-08-24 18:31:24 -0400

    Agregando Hola Mundo y estilos

--- app.js
+++ app.js
@@ -2,9 +2,9 @@ const express = require("express");
 const app = express();
 
 
-app.get("/", (req, res) => {
-    res.end("works!")
+app.use(express.static("public"));
+
+
+app.listen(3000, () => {
+    console.log("servidor iniciado")
 });
-
-
-app.listen(3000);
