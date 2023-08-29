af7efc9083236f28f4cb2b10fa1e8ac569bc3ad8
felipehmora <felipehmoraarg@gmail.com>
2023-08-29 17:54:24 -0400

    practicando con objetos

--- ejemplo.js
+++ ejemplo.js
@@ -2,6 +2,28 @@ console.log("si mi amor");
 console.log("adios mundo");
 
 let operacion = 2 / 2;
-console.log(operacion)
+console.log(operacion);
+
+{
+nombre: "Juan"
+}
+
+
+{
+nombre: "Pedro"
+
+}
+
+console.log(
+{
+nombre: "Juan"
+}
+)
+
+console.log(
+{
+nombre: "Pedro"
+}
+)
 
 require("./ejemplo2.js")
