676d3ab0da6180891ebb2d0b900403b0bed731f1
felipehmora <felipehmoraarg@gmail.com>
2023-08-29 17:19:18 -0400

    mis primeros programas con node

---
+++ ejemplo.js
@@ -0,0 +1,7 @@
+console.log("si mi amor");
+console.log("adios mundo");
+
+let operacion = 2 / 2;
+console.log(operacion)
+
+require("./ejemplo2.js")
---
+++ ejemplo2.js
@@ -0,0 +1,4 @@
+console.log("este es mi primer programa");
+console.log("este es el modulo 2");
+
+console.log("este es mi funcionalidad")
